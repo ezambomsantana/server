1a44b4d1c5cae1b752d73531d97979f7698e6662
Luiz Henrique Zambom Santana <lhzsantana@hotmail.com>
2016-01-02 19:42:57 -0200

    New login

--- src/main/java/com/santana/socialnetwork/server/controllers/UserController.java
+++ src/main/java/com/santana/socialnetwork/server/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.santana.socialnetwork.server.controllers;
 
 import javax.inject.Inject;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,16 @@ import com.santana.socialnetwork.model.User;
 import com.santana.socialnetwork.server.repository.UserRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8000")
 @RequestMapping("/users")
 public class UserController {
 
     @Inject
     UserRepository userRepository;
     
-    @RequestMapping("/loggin")
-    public User loggin(
+    @CrossOrigin(origins = "http://localhost:8000")
+    @RequestMapping("/login")
+    public User login(
     		@RequestParam(value="email") String email,
     		@RequestParam(value="password") String password) {
     	User user = userRepository.findUserByEmailAndPassword(email, password);
